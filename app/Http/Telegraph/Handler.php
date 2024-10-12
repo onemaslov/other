39c4954d98d6c6d4fc80a0d5dc5fefabd1076e9f
onemaslov <onemaslov@gmail.com>
2024-10-12 13:09:13 +0300

    added telegraph

--- app/Http/Telegraph/Handler.php
+++ app/Http/Telegraph/Handler.php
@@ -7,6 +7,10 @@ namespace App\Http\Telegraph;
 use App\Services\TelegramBotService;
 use DefStudio\Telegraph\Facades\Telegraph;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\Keyboard;
+use DefStudio\Telegraph\Models\TelegraphChat;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Stringable;
 
 class Handler extends WebhookHandler
@@ -18,7 +22,28 @@ class Handler extends WebhookHandler
 
     public function start(): void
     {
+        Log::debug(
+            __METHOD__,
+            [
+                'команда старт' => $this->message->toArray()
+            ]
+        );
         $this->reply('Здарова я бот');
+
+//		Telegraph::message('hello world')
+//			->keyboard(Keyboard::make()->buttons([
+//				Button::make('Delete')->action('delete')->param('id', '42'),
+//				Button::make('Login Url')->loginUrl('https://loginUrl.test.it'),
+//			]))->send();
+
+//		Telegraph::message('hello world')
+//			->keyboard(function(Keyboard $keyboard){
+//				return $keyboard
+//					->button('Delete')->action('delete')->param('id', '42')
+//					->button('open')->url('https://test.it')
+//					->button('Web App')->webApp('https://web-app.test.it')
+//					->button('Login Url')->loginUrl('https://loginUrl.test.it');
+//			})->send();
 
 //        Telegraph::message('Спасибо за ответ')->send();
     }
@@ -31,6 +56,32 @@ class Handler extends WebhookHandler
     protected function handleChatMessage(Stringable $text): void
     {
 //        $this->message;
+        $chat = $this->message->getChat();
+        $telegraphChat = TelegraphChat::where('chat_id', $chat->id)->first();
+
+        if (!$telegraphChat) {
+            TelegraphChat::create([
+                'chat_id' => $chat->id,
+                'name' => $chat->name,
+                'telegraph_bot_id' => 1
+            ]);
+        }
+
+        Log::debug(
+            __METHOD__,
+            [
+                'инфа о сообщении' => $this->message->toArray()
+            ]
+        );
+
+        Telegraph::message('hello world')
+            ->keyboard(function(Keyboard $keyboard){
+                return $keyboard
+                    ->button('Delete')->action('delete')->param('id', '42')
+                    ->button('open')->url('https://test.it')
+                    ->button('Web App')->webApp('https://web-app.test.it')
+                    ->button('Login Url')->loginUrl('https://loginUrl.test.it');
+            })->send();
     }
 
     public function subscribe(): void
---
+++ app/Models/TelegraphChat.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class TelegraphChat extends Model
+{
+    use HasFactory, Notifiable;
+
+    protected $guarded = [];
+}
